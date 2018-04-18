6f4966b82565769e533c5deef4192bfe40e7faa6
sundhhy <418755805@qq.com>
2018-04-18 16:22:49 +0800

    WXserial的界面可以使用了

--- WXSerial/wxserial.py
+++ WXSerial/wxserial.py
@@ -9,17 +9,26 @@
 
 import wx
 import wx.xrc
-
+import serial.tools.list_ports
 ###########################################################################
 ## Class SerialFrameUI
 ###########################################################################
+
+def SER_Get_available_com_name():
+    plist = list(serial.tools.list_ports.comports())
+    name_list = []
+    for list_ in plist:
+        available_com = list_[0]
+        name_list.append(available_com)
+
+    return name_list
 
 class SerialFrameUI ( wx.Frame ):
 	
 	def __init__( self, parent ):
 		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 574,456 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 		
-		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
+		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 		self.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_INFOBK ) )
 		self.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOWFRAME ) )
 		
@@ -54,13 +63,15 @@ class SerialFrameUI ( wx.Frame ):
 		
 		bSizer1.Add( bSizer3, 0, wx.EXPAND, 5 )
 		
-		bSizer4 = wx.BoxSizer( wx.HORIZONTAL )
+		#bSizer4 = wx.BoxSizer( wx.HORIZONTAL )
+		bSizer4 = wx.BoxSizer(wx.HORIZONTAL)
 		
 		self.m_lblCOMX = wx.StaticText( self, wx.ID_ANY, u"串口号：", wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.m_lblCOMX.Wrap( -1 )
 		bSizer4.Add( self.m_lblCOMX, 0, wx.ALL, 5 )
 		
-		m_cmbCOMXChoices = [ u"COM1", u"COM2", u"COM3", u"COM4", u"COM5", u"COM6", u"COM7", u"COM8", u"COM9" ]
+		#m_cmbCOMXChoices = [ u"COM1", u"COM2", u"COM3", u"COM4", u"COM5", u"COM6", u"COM7", u"COM8", u"COM9" ]
+		m_cmbCOMXChoices =  SER_Get_available_com_name()
 		self.m_cmbCOMX = wx.ComboBox( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, m_cmbCOMXChoices, wx.CB_READONLY )
 		self.m_cmbCOMX.SetSelection( 0 )
 		self.m_cmbCOMX.SetMinSize( wx.Size( 100,-1 ) )
@@ -71,7 +82,7 @@ class SerialFrameUI ( wx.Frame ):
 		self.m_lblBaud.Wrap( -1 )
 		bSizer4.Add( self.m_lblBaud, 0, wx.ALL, 5 )
 		
-		m_cmbBaudChoices = [ u"2400", u"4800", u"9600", u"14400", u"19200", u"28800", u"57600" ]
+		m_cmbBaudChoices = [ u"2400", u"4800", u"9600", u"14400", u"19200", u"28800", u"57600",  u"115200" ]
 		self.m_cmbBaud = wx.ComboBox( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, m_cmbBaudChoices, wx.CB_READONLY )
 		self.m_cmbBaud.SetSelection( 6 )
 		self.m_cmbBaud.SetMinSize( wx.Size( 100,-1 ) )
@@ -82,7 +93,8 @@ class SerialFrameUI ( wx.Frame ):
 		bSizer4.Add( self.m_btnOpen, 8, wx.ALL, 5 )
 		
 		
-		bSizer4.AddSpacer( ( 0, 0), 1, wx.EXPAND, 5 )
+		#bSizer4.AddSpacer( (0, 0), 1, wx.EXPAND, 5 )
+		#bSizer4.Add((10, 10), (0, 0))
 		
 		self.m_imgStat = wx.StaticBitmap( self, wx.ID_ANY, wx.ArtProvider.GetBitmap( wx.ART_HELP, wx.ART_BUTTON ), wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.m_imgStat.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOWFRAME ) )
@@ -90,7 +102,7 @@ class SerialFrameUI ( wx.Frame ):
 		bSizer4.Add( self.m_imgStat, 0, wx.ALL, 5 )
 		
 		
-		bSizer4.AddSpacer( ( 0, 0), 1, wx.EXPAND, 5 )
+		#bSizer4.AddSpacer( ( 0, 0), 1, wx.EXPAND, 5 )
 		
 		self.m_chkHEXSend = wx.CheckBox( self, wx.ID_ANY, u"HEX发送", wx.DefaultPosition, wx.DefaultSize, 0 )
 		bSizer4.Add( self.m_chkHEXSend, 0, wx.ALL, 5 )
@@ -137,7 +149,7 @@ class SerialFrameUI ( wx.Frame ):
 		bSizer5.Add( self.m_btnExtn, 0, wx.ALL, 5 )
 		
 		
-		bSizer5.AddSpacer( ( 10, 0), 1, wx.EXPAND, 5 )
+		#bSizer5.AddSpacer( ( 10, 0), 1, wx.EXPAND, 5 )
 		
 		
 		bSizer1.Add( bSizer5, 0, wx.EXPAND, 5 )
@@ -187,64 +199,68 @@ from wx.lib.pubsub import pub
 
 
 class SerialFrame(SerialFrameUI):
-    def __init__(self,parent=None):
-        super(SerialFrame,self).__init__(parent)
-        
-        self.m_imgStat.SetBitmap(Img_inclosing.getBitmap())
-        
-        self.Ser = Serial()
-        self.serialThread = SerialThread(self.Ser)
-        
-        #create a pubsub receiver
-        pub().subscribe(self.on_txtMain_update,'update')
-        
-    def on_txtMain_update(self, msg):
-        self.m_txtMain.AppendText(msg.data)
-    
-    def on_btnSend_clicked( self, event ):
-        self.Ser.write(self.m_txtInput.GetValue())
-        
-    def on_btnClear_clicked( self, event ):
-        self.m_txtMain.Clear()
-    
-    def on_chkHEXShow_changed( self, event ):
-        s = self.m_txtMain.GetValue()
-        if self.m_chkHEXShow.IsChecked():
-            s = ''.join('%02X' %i for i in [ord(c) for c in s])
-        else:
-            s = ''.join([chr(int(i,16)) for i in [s[i*2:i*2+2] for i in range(0,len(s)/2)]])
-        self.m_txtMain.Clear()
-        self.m_txtMain.SetValue(s)
-        
-    def on_cmbBaud_changled( self, event ):
-        self.Ser.setBaudrate(int(self.m_cmbBaud.GetValue()))
-        
-    def on_btnOpen_clicked( self, event ):
-        if not self.Ser.isOpen():
-            try:
-                self.Ser.timeout = 1
-                self.Ser.xonxoff = 0
-                self.Ser.port = self.m_cmbCOMX.GetValue()
-                self.Ser.parity = self.m_cmbChek.GetValue()[0]
-                self.Ser.baudrate = int(self.m_cmbBaud.GetValue())
-                self.Ser.bytesize = int(self.m_cmbData.GetValue())
-                self.Ser.stopbits = int(self.m_cmbStop.GetValue())
-                self.Ser.open()
-            except Exception as e:
-                print('COMM Open Fail!!',e)
-            else:
-                self.m_btnOpen.SetLabel(u'关闭串口')
-                self.m_imgStat.SetBitmap(Img_inopening.getBitmap())
-        else:
-            self.Ser.close()
-            while self.Ser.isOpen(): pass
+	def __init__(self,parent=None):
+		super(SerialFrame,self).__init__(parent)
 
-            self.m_btnOpen.SetLabel(u'打开串口')
-            self.m_imgStat.SetBitmap(Img_inclosing.getBitmap())
+		self.m_imgStat.SetBitmap(Img_inclosing.getBitmap())
 
-    
-    def on_btnExtn_clicked( self, event ):
-        event.Skip()
+		self.Ser = Serial()
+		self.serialThread = SerialThread(self.Ser)
+		#create a pubsub receiver
+		#sdh
+		#pub().subscribe(self.on_txtMain_update,'update')
+        
+	def on_txtMain_update(self, msg):
+		self.m_txtMain.AppendText(msg.data)
+
+	def on_btnSend_clicked( self, event ):
+		self.Ser.write(self.m_txtInput.GetValue())
+
+	def on_btnClear_clicked( self, event ):
+		self.m_txtMain.Clear()
+
+	def on_chkHEXShow_changed( self, event ):
+		s = self.m_txtMain.GetValue()
+		if self.m_chkHEXShow.IsChecked():
+			s = ''.join('%02X' %i for i in [ord(c) for c in s])
+		else:
+			s = ''.join([chr(int(i,16)) for i in [s[i*2:i*2+2] for i in range(0,len(s)/2)]])
+		self.m_txtMain.Clear()
+		self.m_txtMain.SetValue(s)
+
+	def on_cmbBaud_changled( self, event):
+		print('fuck')
+		baud = int(self.m_cmbBaud.GetValue())
+		print(baud)
+		#self.Ser.setBaudrate(int(self.m_cmbBaud.GetValue()))
+		self.Ser.baudrate = baud
+
+	def on_btnOpen_clicked( self, event ):
+		if not self.Ser.isOpen():
+			try:
+				self.Ser.timeout = 1
+				self.Ser.xonxoff = 0
+				self.Ser.port = self.m_cmbCOMX.GetValue()
+				self.Ser.parity = self.m_cmbChek.GetValue()[0]
+				self.Ser.baudrate = int(self.m_cmbBaud.GetValue())
+				self.Ser.bytesize = int(self.m_cmbData.GetValue())
+				self.Ser.stopbits = int(self.m_cmbStop.GetValue())
+				self.Ser.open()
+			except Exception as e:
+				print('COMM Open Fail!!',e)
+		else:
+			self.m_btnOpen.SetLabel(u'关闭串口')
+			self.m_imgStat.SetBitmap(Img_inopening.getBitmap())
+		#else:
+			self.Ser.close()
+			while self.Ser.isOpen(): pass
+
+		self.m_btnOpen.SetLabel(u'打开串口')
+		self.m_imgStat.SetBitmap(Img_inclosing.getBitmap())
+
+
+	def on_btnExtn_clicked( self, event ):
+		event.Skip()
 
 
 
@@ -291,7 +307,7 @@ Img_inopening = embeddedimage.PyEmbeddedImage(
 
 
 if __name__ == '__main__':
-    app = wx.PySimpleApp()
+    app = wx.App()
     frame = SerialFrame()
     frame.Show(True)
     app.MainLoop()
